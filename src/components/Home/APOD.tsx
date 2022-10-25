53a412999110ee7911e57b841204e7eb5446e8dd
Ethan Farrell <78605366+Trailblazer780@users.noreply.github.com>
2022-10-25 10:23:58 -0300

    responsive iframe on APOD

--- src/components/Home/APOD.tsx
+++ src/components/Home/APOD.tsx
@@ -1,5 +1,5 @@
 import { APODData } from "../../tools/data.model";
-
+import './APOD.css';
 type Props ={
     data:APODData,
     setLoading: React.Dispatch<React.SetStateAction<any>>;
@@ -18,13 +18,19 @@ const APOD:React.FC<Props> = (props) => {
                 </div>
             </div>
             <div className="relative blur-everything flex justify-center py-8 animate__animated animate__fadeIn animate__delay-3s z-20">
-                <div className="px-10 py-10 w-max rounded-xl shadow-lg border bg-gray-600 border-primary-500 transition duration-700 hover:scale-125">
-                    {props.data.media_type === "image" ? <a href={props.data.hdurl} target="_blank" rel="noreferrer"><img className="" alt="" src={props.data.url} onLoad={() => {props.setLoading(false)}}/></a> : <iframe className="APOD" width="853" height="505" src="https://www.youtube.com/embed/NS71ppsk7n0?rel=0" onLoad={() => {props.setLoading(false)}}> </iframe>}
-                </div>
+                {props.data.media_type === "image" &&
+                    <div className="px-10 py-10 w-max rounded-xl shadow-lg border bg-gray-600 border-primary-500 transition duration-700 hover:scale-125">
+                        <a href={props.data.hdurl} target="_blank" rel="noreferrer"><img className="" alt="" src={props.data.url} onLoad={() => {props.setLoading(false)}}/></a>
+                    </div>}
+                {props.data.media_type === "video" &&
+                    <div className="px-10 py-10 w-max rounded-xl shadow-lg border bg-gray-600 border-primary-500 transition duration-700 containeriframe">
+                        <iframe className="responsive-iframe" title={props.data.title}  src={props.data.url} onLoad={() => {props.setLoading(false)}}> </iframe>
+                    </div>}
             </div>
             <div className="relative flex flex-col justify-center items-center py-2 animate__animated animate__fadeIn animate__delay-3s">
                 <div className="px-6 pb-8 w-full rounded-xl shadow-lg border bg-gray-700 border-primary-500">
-                    <h2 className="text-center m-2 text-4xl text-primary-500">Photo Explaination</h2>
+                    {props.data.media_type === "image" && <h2 className="text-center m-2 text-4xl text-primary-500">Photo Explaination</h2>}    
+                    {props.data.media_type === "video" && <h2 className="text-center m-2 text-4xl text-primary-500">Video Explaination</h2>}
                     <p className="text-white text-xl ">{props.data.explanation}</p>
                 </div>
             </div>
